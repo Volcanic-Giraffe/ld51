356aaace2aa8b4433465dc3974680e590040459c
A333 <arz.giyan@gmail.com>
2022-10-01 16:08:45 +0300

    Road tiles

---
+++ Assets/Scripts/RoadSwitch.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class RoadSwitch : MonoBehaviour
+{
+    public Vector2Int Direction;
+    private ConnectionType connection;
+
+    public void Toggle()
+    {
+        if (Direction == Vector2Int.up)
+        {
+            Direction = connection == ConnectionType.TripleDLU ? Vector2Int.down : Direction = Vector2Int.right;
+        }
+        else if (Direction == Vector2Int.right)
+        {
+            Direction = connection == ConnectionType.TripleLUR ? Vector2Int.left : Direction = Vector2Int.down;
+        }
+        else if (Direction == Vector2Int.down)
+        {
+            Direction = connection == ConnectionType.TripleURD ? Vector2Int.up : Vector2Int.left;
+        }
+        else
+        {
+            Direction = connection == ConnectionType.TripleRDL ? Vector2Int.right : Vector2Int.up;
+        }
+        // TODO ROTATE ARROW
+    }
+    internal void SetAllowedConnections(ConnectionType conn)
+    {
+        if (connection != ConnectionType.TripleURD && connection != ConnectionType.TripleDLU
+            && connection != ConnectionType.TripleLUR && connection != ConnectionType.TripleRDL
+            && connection != ConnectionType.Quad) throw new Exception($"Switch can only operate on triple/quad connectors, {gameObject}");
+        this.connection = conn;
+    }
+}
---
+++ Assets/Scripts/RoadTile.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ConnectionType
+{
+    Horizontal, Vertical, DiagLU, DiagUR, DiagRD, DiagDL, TripleLUR, TripleURD, TripleRDL, TripleDLU, Quad
+}
+
+public class RoadTile : MonoBehaviour
+{
+    public ConnectionType Type;
+    public RoadSwitch RoadSwitch;
+
+    private void Awake()
+    {
+        RoadSwitch.SetAllowedConnections(Type);
+    }
+    public Vector2Int Direction(Vector2Int from, Vagon vagon = null)
+    {
+        switch (Type)
+        {
+            case ConnectionType.Horizontal:
+                return new Vector2Int(from.x * -1, 0);
+            case ConnectionType.Vertical:
+                return new Vector2Int(0, from.y * -1);
+            case ConnectionType.DiagDL:
+                if (from == Vector2Int.down) return Vector2Int.left;
+                if (from == Vector2Int.left) return Vector2Int.down;
+                break;
+            case ConnectionType.DiagRD:
+                if (from == Vector2Int.down) return Vector2Int.right;
+                if (from == Vector2Int.right) return Vector2Int.down;
+                break;
+            case ConnectionType.DiagLU:
+                if (from == Vector2Int.left) return Vector2Int.up;
+                if (from == Vector2Int.up) return Vector2Int.left;
+                break;
+            case ConnectionType.DiagUR:
+                if (from == Vector2Int.up) return Vector2Int.right;
+                if (from == Vector2Int.right) return Vector2Int.up;
+                break;
+            // TODO control by click
+            case ConnectionType.TripleDLU:
+                if (RoadSwitch.Direction != from) return RoadSwitch.Direction;
+                break;
+        }
+        // cant go!
+        return Vector2Int.zero;
+    }
+
+    public static Dictionary<ConnectionType, Vector2Int[]> AllowedDirections = new Dictionary<ConnectionType, Vector2Int[]>()
+    {
+        { ConnectionType.Horizontal, new Vector2Int[] { Vector2Int.up, Vector2Int.down } },
+        { ConnectionType.Vertical, new Vector2Int[] { Vector2Int.left, Vector2Int.right } },
+        { ConnectionType.DiagDL, new Vector2Int[] { Vector2Int.down, Vector2Int.left} },
+        { ConnectionType.DiagLU, new Vector2Int[] { Vector2Int.left, Vector2Int.up} },
+        { ConnectionType.DiagRD, new Vector2Int[] { Vector2Int.right, Vector2Int.down} },
+        { ConnectionType.DiagUR, new Vector2Int[] { Vector2Int.up, Vector2Int.right} },
+        { ConnectionType.TripleDLU, new Vector2Int[] { Vector2Int.up, Vector2Int.left, Vector2Int.down} },
+        { ConnectionType.TripleLUR, new Vector2Int[] { Vector2Int.left, Vector2Int.up, Vector2Int.right} },
+        { ConnectionType.TripleRDL, new Vector2Int[] { Vector2Int.right, Vector2Int.down, Vector2Int.left} },
+        { ConnectionType.TripleURD, new Vector2Int[] { Vector2Int.up, Vector2Int.right, Vector2Int.down} },
+        { ConnectionType.Quad, new Vector2Int[] { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left} },
+    };
+}
+
