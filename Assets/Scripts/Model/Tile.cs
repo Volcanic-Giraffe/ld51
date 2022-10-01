cf48bba8ed3a41c2698c8aa178f21737d44f87c3
Alexey Grishin <alexey.grishin@gmail.com>
2022-10-01 19:17:47 +0600

    модельковое

---
+++ Assets/Scripts/Model/Tile.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+// Суть есть место в гриде
+public class Tile : MonoBehaviour
+{
+    public bool Highlighted;
+    public bool Busy;
+
+    public ITileElement Element;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        var tileElement = other.GetComponent<ITileElement>();
+        if (tileElement != null)
+        {
+            Busy = true;
+            Element = tileElement;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var tileElement = other.GetComponent<ITileElement>();
+        if (tileElement != null)
+        {
+            Busy = false;
+            Element = null;
+        }
+
+    }
+
+}
