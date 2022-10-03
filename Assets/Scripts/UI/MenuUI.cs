0c7abdca0e2a25fe4da904e5d8da213b38c95085
Ruslan Kibets <ruslan.kibets@gmail.com>
2022-10-03 12:13:32 -0700

    Esc to show/hide menu

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -9,17 +10,39 @@ public class MenuUI : MonoBehaviour
     [SerializeField] private RectTransform container;
     [SerializeField] private Image overlay;
 
+    private bool _shown;
+
+    private void Awake()
+    {
+        _shown = true;
+    }
 
     public void Show()
     {
+        _shown = true;
         container.gameObject.SetActive(true);
         
         GameController.Instance.SetPaused(true);
     }
-    public void OnAnywhereClicked()
+
+    public void Hide()
     {
+        _shown = false;
         container.gameObject.SetActive(false);
 
         GameController.Instance.SetPaused(false);
+    }
+    
+    public void OnAnywhereClicked()
+    {
+        Hide();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_shown) Hide(); else Show();
+        }
     }
 }
